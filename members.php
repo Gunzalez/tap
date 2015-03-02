d81aaddb7f54a50748843105939dcaed3ad8723b
Segun Konibire <gunzalez@hotmail.com>
2015-03-02 22:49:55 +0000

    Minor refactor, Members page at 80%

--- members.php
+++ members.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 
-    <title>Tea Advisory Panel (TAP)</title>
+    <title>Tea Advisory Panel (TAP) - Members</title>
     <?php include_once('includes/resources.php'); ?>
 
 </head>
-<body id="home">
+<body id="members">
     <div class="header">
         <div class="container">
             <a href="#mobileNav" id="mobileNavButton"><i class="fa fa-fw"></i></a>
@@ -55,164 +55,85 @@
                     <li><a href="/contact/">Contact</a></li>
                 </ul>
             </div>
-            <div class="carousel">
-                <div class="imagery">
-                    <ul id="header-carousel">
-                        <li>
-                            <img src="uploaded/carousel-slide-1.jpg" alt="slide 1" />
-                            <div class="slide-copy">
-                                <h2>Is it important?</h2>
-                                <p>Tea is the most commonly consumed beverage in the world after water. In the UK it has been drunk for over 350 years. Today, 77% of British adults drink tea and of the tea consumed in the UK, 95% is black tea.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="uploaded/carousel-slide-3.jpg" alt="slide 2" />
-                            <div class="slide-copy">
-                                <h2>Golden!</h2>
-                                <p>Morbi lectus riis, luctus non, massa. Fusce ac turpis quis ligsus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="uploaded/carousel-slide-2.jpg" alt="slide 3" />
-                            <div class="slide-copy">
-                                <h2>Tea With Love</h2>
-                                <p>Sunday morning, I'm out in th street dancing like a mad man, until the police come to take me away, I hate it when they do that.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="uploaded/carousel-slide-4.jpg" alt="slide 4" />
-                            <div class="slide-copy">
-                                <h2>He drinks it too</h2>
-                                <p>And then he reads your emails, only kidding  Suspendisse potenti. Nunc feugiat mi a equat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="uploaded/carousel-slide-5.jpg" alt="slide 5" />
-                            <div class="slide-copy">
-                                <h2>Fields of green!</h2>
-                                <p>Not that green you naughty boy, I mean tea. Green fields of tea plants everywhere. tra auctor, sem massa mattis sem, at interdum magna augue eget diam. </p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="copy">
-                    <div id="carousel-description" class="description"></div>
-                </div>
-            </div>
             <div class="cleft"></div>
+            <div class="bodyBorder"></div>
         </div>
     </div>
     <div class="body">
         <div class="container">
-            <div class="faqs">
-                <div class="title">
-                    <h2>FAQs</h2>
-                </div>
-                <a href="#" class="copy">
-                    <span class="question">
-                        <span class="quotes">&ldquo;</span><span class="text">Is drinking tea bad for the bones?</span><span class="quotes">&rdquo;</span>
-                    </span>
-                </a>
-                <div class="cboth"></div>
-            </div>
-        </div>
-        <div class="grey-bar">
-            <div class="container">
-                <h2 class="title">What we do.</h2>
-                <p>The Tea Advisory Panel is an expert organisation which can:</p>
+            <div class="page-padding">
+                <h2 class="title">Tap members</h2>
                 <ul>
-                    <li>
-                        <a href="#">
-                            <span class="item">
-                                <span class="icon"><i class="fa fa-fw"></i></span>
-                                <span class="intro">Provide independent and objective information about the latest health benefits regarding black tea.</span>
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span class="item">
-                                <span class="icon"><i class="fa fa-fw"></i></span>
-                                <span class="intro">Answer all of your nutrition and hydration questions about black tea.</span>
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span class="item">
-                                <span class="icon"><i class="fa fa-fw"></i></span>
-                                <span class="intro">Set the record straight on ‘science’ myths associated with black tea.</span>
-                            </span>
-                        </a>
-                    </li>
+                    <li>Provide independent and objective information about the latest health benefits regarding black tea</li>
+                    <li>Answer all of your nutrition and hydration questions about black tea</li>
+                    <li>Set the record straight on ‘science’ myths associated with black tea</li>
                 </ul>
-                <div class="cleft"></div>
+                <p>TAP is a novel health group, bringing together experts in the areas of public health, general practice, nutrition and diet. Its objective is to provide informed ‘advice’ about the essential health, hydration and dietary role that black tea can provide in our daily diets.</p>
+                <p>It also as a group will be commissioning a series of study reviews and market research initiatives investigating further the ‘natural’ well-being benefits associated with black tea. TAP is supported by an unrestricted educational grant from the UK Tea Council, the trade association for the UK tea industry.</p>
+                <p>TAP members include nutritionists, dieticians and doctors.</p>
             </div>
-        </div>
-        <div class="container">
-            <ul id="body-carousel" class="carousel">
-                <li class="item">
-                    <div class="imagery">
-                        <img src="uploaded/dr-catherine-hood.png" alt="" />
-                    </div>
-                    <div class="copy">
-                        <h2>Who we are</h2>
-                        <p class="sub-title">Dr Catherine Hood</p>
-                        <p class="details">Dr Catherine Hood is a health broadcaster and writer. She presented Channel Five’s prime time series A Girl's Guide to 21st Century Sex and regularly appears on The Wright Stuff discussing all aspects of general medicine.</p>
-                        <div class="buttons">
-                            <a href="#" class="button next">Read&nbsp;More</a><br />
-                            <a href="#" class="button next">Other&nbsp;Members</a>
-                        </div>
-                    </div>
-                    <div class="cboth"></div>
+            <ul class="members-list">
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-1.jpg" alt="" />
+                        <span>Dr Aku</span>
+                    </a>
                 </li>
-                <li class="item">
-                    <div class="imagery">
-                        <img src="uploaded/dr-pretty-woman.png" alt="" />
-                    </div>
-                    <div class="copy">
-                        <h2>Who we are</h2>
-                        <p class="sub-title">Dr Pretty Lady</p>
-                        <p class="details">Dr Pretty Lady is an exercise physiologist with a scientific research background. She is currently a Professor in Ageing and Health at Glasgow Caledonian University.  She is Deputy Lead of the Later Life Research Group and also the Scottish Centre for Evidence Based Care. </p>
-                        <div class="buttons">
-                            <a href="#" class="button next">Read&nbsp;More</a><br />
-                            <a href="#" class="button next">Other&nbsp;Members</a>
-                        </div>
-                    </div>
-                    <div class="cboth"></div>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-2.jpg" alt="" />
+                        <span>Dr Aku 2</span>
+                    </a>
                 </li>
-                <li class="item">
-                    <div class="imagery">
-                        <img src="uploaded/dr-who.png" alt="" />
-                    </div>
-                    <div class="copy">
-                        <h2>Who we are</h2>
-                        <p class="sub-title">Dr Who</p>
-                        <p class="details">Doctor Who is a British science-fiction television programme produced by the BBC from 1963 to the present day. The programme depicts the adventures of the Doctor, a Time Lord—a time-travelling humanoid alien. He explores the universe in his TARDIS, a sentient time-travelling space ship.</p>
-                        <div class="buttons">
-                            <a href="#" class="button next">Read&nbsp;More</a><br />
-                            <a href="#" class="button next">Other&nbsp;Members</a>
-                        </div>
-                    </div>
-                    <div class="cboth"></div>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-3.jpg" alt="" />
+                        <span>Mr Aku</span>
+                    </a>
                 </li>
-                <li class="item">
-                    <div class="imagery">
-                        <img src="uploaded/dr-typical-hood.png" alt="" />
-                    </div>
-                    <div class="copy">
-                        <h2>Who we are</h2>
-                        <p class="sub-title">Dr Typical Doctor</p>
-                        <p class="details">Dr Typical Doctor is a fictional character, a mercenary and anti-hero appearing in comic books published by Marvel Comics. Created by writer and artist Rob Liefeld and scripted by Fabian Nicieza.</p>
-                        <div class="buttons">
-                            <a href="#" class="button next">Read&nbsp;More</a><br />
-                            <a href="#" class="button next">Other&nbsp;Members</a>
-                        </div>
-                    </div>
-                    <div class="cboth"></div>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-1.jpg" alt="" />
+                        <span>Master Aku</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-3.jpg" alt="" />
+                        <span>Dr Aku</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-2.jpg" alt="" />
+                        <span>Dr Aku 2</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-1.jpg" alt="" />
+                        <span>Mr Aku</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="uploaded/members-3.jpg" alt="" />
+                        <span>Master Aku</span>
+                    </a>
                 </li>
             </ul>
+            <div class="cleft"></div>
+            <div class="page-padding">
+                <h3>Our objectives are</h3>
+                <ul>
+                    <li>To be an independent and impartial source of information, focusing of the health and nutrition benefits associated with black tea</li>
+                    <li>To explain the latest science findings in relation to black tea</li>
+                    <li>To put into place and review new research investigating people’s general nutrition and lifestyle habits, plus the associated health outcomes of black tea consumption</li>
+                    <li>To de-mystify health issues surrounding black tea</li>
+                </ul>
+            </div>
         </div>
+
         <div class="container">
             <div class="forums">
                 <div class="forum-chats">
@@ -263,6 +184,16 @@
                 <div class="cboth"></div>
             </div>
         </div>
+
+        <div class="grey-bar">
+            <div class="container">
+
+
+
+
+            </div>
+        </div>
+
     </div>
     <div class="footer">
         <div class="container">
